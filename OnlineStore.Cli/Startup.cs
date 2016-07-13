7c8480e443c7ee916e66d05071a2608c64a18337
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-12 21:10:40 -0400

    initial commit

---
+++ OnlineStore.Cli/Startup.cs
@@ -0,0 +1,18 @@
+using OnlineStore;
+using Owin;
+using System.Web.Http;
+using Unity.WebApi;
+
+namespace OnlineStore.Cli
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.DependencyResolver = new UnityDependencyResolver(UnityConfiguration.GetContainer());
+            ApiConfiguration.Install(config, app);
+            app.UseWebApi(config);
+        }
+    }
+}
